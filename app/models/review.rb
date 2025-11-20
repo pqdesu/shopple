32788d11baa505b4764e7fb09b573aef0c6163a8
pedro sassaki <pedropq2001@gmail.com>
2025-11-21 00:14:10 +0900

    validations done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  # --- Validations ---
+  validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :comment, length: { maximum: 500}
 end
