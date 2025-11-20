a292ea3dd96437b9ab6b58779c972b63cd08c8c8
pedro sassaki <pedropq2001@gmail.com>
2025-11-21 00:00:17 +0900

    validations done

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  # --- Validations ---
+  validates :quantity, presence: true, numericality: { greater_than: 0}
+  validates :price, presence: true
 end
