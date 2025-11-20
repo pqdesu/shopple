339a9208c5ca6c735037357b595040a23a9cfef6
pedro sassaki <pedropq2001@gmail.com>
2025-11-20 23:00:57 +0900

    associations done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+
+  # Associations
+  has_many :reviews, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :order_items
 end
