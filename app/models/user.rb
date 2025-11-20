9db05fb15adb85981b11762b3ecd47c854374736
pedro sassaki <pedropq2001@gmail.com>
2025-11-20 22:59:41 +0900

    associations done and added acts_as_favoritor

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Associations
+  has_one :cart, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
+  # --- Gem: acts_as_favoritor ---
+  acts_as_favoritor
+
+
+
 end
