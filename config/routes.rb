db7210341811d183b81909427fe40e923d03b674
pedro sassaki <pedropq2001@gmail.com>
2025-11-24 23:40:41 +0900

    root to products/index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
+
+  resources :products, only: %i[index show] do
+    # Nesting favorites inside products makes the URL: /products/1/favorites
+    resources :favorites, only: %i[create destroy]
+  end
+
+  # View for seeing all favorites at once
+  resources :favorites, only: [:index]
+
+  resource :cart, only: [:show]
+  resources :cart_items, only: %i[create update destroy]
+  resources :orders, only: %i[index show create]
 end
