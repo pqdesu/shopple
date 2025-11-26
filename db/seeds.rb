f0338b0d8237a179757943558bad88a515e0b9e3
pedro sassaki <pedropq2001@gmail.com>
2025-11-26 15:01:58 +0900

    new testing seed file without category_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,24 @@ require "json"
 puts "Cleaning the database..."
 
 OrderItem.destroy_all
+puts "Deleted OrderItem"
 CartItem.destroy_all
+puts "Deleted CartItem"
 Product.destroy_all
-Category.destroy_all
+puts "Deleted Products"
 puts "..."
 
 # Get data from API
 puts "Fetching data from FakeStoreAPI..."
-
-response = URI.open("https://fakestoreapi.com/products").read
-products_data = JSON.parse(response)
+products_data = JSON.parse(URI.open("https://fakestoreapi.com/products").read)
 puts "..."
 
 puts "Creating Categories and Products"
-
 #  Category must exist, Price can't be blank, Price is not a number, Category can't be blank
 products_data.each do |product|
-  category_data = Category.find_or_create_by!(name: product["category"])
   Product.create!(
     name: product["title"],
-    category: category_data,
+    category: product["category"],
     price: product["price"]
   )
   puts "Created #{product["title"]}"
@@ -32,6 +30,5 @@ end
 
 puts "------------------------------------------------"
 puts "SEEDING COMPLETE!"
-puts "Categories created: #{Category.count}"
 puts "Products created:   #{Product.count}"
 puts "------------------------------------------------"
