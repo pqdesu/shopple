94c99ec18f2785054624fcdd0e1604ccd63d31fc
pedro sassaki <pedropq2001@gmail.com>
2025-12-17 11:28:59 +0900

    feat: add interactive size selection buttons on product page

---
+++ app/javascript/controllers/product_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    this.element.querySelectorAll(".size-btn").forEach((button) => {
+      button.addEventListener("click", () => {
+        button.classList.toggle("selected");
+      });
+    });
+  }
+}
