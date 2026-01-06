8b63042c4f5dae592754c49de26831bd76f763a6
pedro sassaki <pedropq2001@gmail.com>
2026-01-06 22:06:36 +0900

    feat: increase displayed products limit from 4 to 6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @new_arrivals = Product.order(created_at: :desc).limit(4)
-    @hot_items = Product.order(rating_rate: :desc).limit(4)
+    @new_arrivals = Product.order(created_at: :desc).limit(6)
+    @hot_items = Product.order(rating_rate: :desc).limit(6)
   end
 end
